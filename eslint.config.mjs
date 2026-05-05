6c2020270316705f840a25acfece9b2430cc9746
Liam Hughes <liam.hughes@octopus.com>
2026-05-05 15:38:31 +1000

    Fix linting and formatting npm scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,9 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  prettier,
-);
+  export default tseslint.config(                                                                                                                                                                                                                                              
+    { ignores: ["**/dist/**"] },                                                                                                                                                                                                                                               
+    eslint.configs.recommended,                                                                                                                                                                                                                                                
+    tseslint.configs.recommended,
+    prettier,                                                                                                                                                                                                                                                                  
+  );  
