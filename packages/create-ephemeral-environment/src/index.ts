d6232efc31122ad2c32362ae8f28e0d4d3b97986
Liam Hughes <liam.hughes@octopus.com>
2026-05-06 14:30:59 +1000

    fix: Remove console.log

--- packages/create-ephemeral-environment/src/index.ts
+++ packages/create-ephemeral-environment/src/index.ts
@@ -3,8 +3,6 @@ import { ActionContextImplementation } from './ActionContextImplementation';
 import { createEnvironment } from './createEnvironment';
 
 createEnvironment(new ActionContextImplementation()).catch((error) => {
-  console.log('❓ Does Release Please build dist?');
-
   setFailed(error);
   process.exit(1);
 });
