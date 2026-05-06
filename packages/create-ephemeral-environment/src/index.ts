07b3bcbeb9e0141747247ab2e9b63863ad4d534c
Liam Hughes <liam.hughes@octopus.com>
2026-05-06 11:33:42 +1000

    feat: Ask a Release Please question

--- packages/create-ephemeral-environment/src/index.ts
+++ packages/create-ephemeral-environment/src/index.ts
@@ -3,6 +3,8 @@ import { ActionContextImplementation } from './ActionContextImplementation';
 import { createEnvironment } from './createEnvironment';
 
 createEnvironment(new ActionContextImplementation()).catch((error) => {
+  console.log('❓ Does Release Please build dist?');
+
   setFailed(error);
   process.exit(1);
 });
